c8d4e9034fde10da3ba3dda3e8f760f4b0f4b515
mateoghidini1998 <mateghidini123@gmail.com>
2021-04-30 13:00:02 -0300

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from blog.models import Category, Article
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+@login_required(login_url='login')
+def list(request):
+
+    #Sacar articulos
+    articles = Article.objects.all()
+
+    #Paginar articulos
+    paginator = Paginator(articles, 5)
+
+    #Recoger numero de pagina
+    page= request.GET.get('page')
+    page_articles = paginator.get_page(page)
+
+    return render(request, 'articles/list.html',{
+        'title': 'Articulos',
+        'articles': page_articles
+    })
+
+@login_required(login_url='login')
+def category(request, category_id):
+    
+    category = get_object_or_404(Category, id=category_id)
+    #articles = Article.objects.filter(categories = category_id)
+
+    return render(request, 'categories/category.html', {
+        'category': category
+        #'articles':articles
+    })
+
+@login_required(login_url='login')
+def article(request, article_id):
+
+    article= get_object_or_404(Article, id=article_id)
+
+    return render(request, 'articles/article.html',{
+        'article': article
+    })
---
+++ pages/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Page
+
+
+#Configuracion extra para mi modelo
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('visible',)
+    list_display = ('title', 'visible', 'created_at')
+    ordering = ('-created_at',)
+
+# Register your models here.
+admin.site.register(Page, PageAdmin)
+
+
+
+#Configuracion del panel de admin
+title = "Proyecto con Django"
+subtitle = "Panel de gestión"
+
+admin.site.site_header = title
+admin.site.site_title = title
+admin.site.index_title = subtitle
